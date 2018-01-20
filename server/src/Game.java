aeea187b893d0b1e069bc876553df68fd2081ddf
Elias Kia <e.kia@gmx.de>
2018-01-20 12:55:58 +0100

    erste einrichtung der Klasse Game

--- server/src/Game.java
+++ server/src/Game.java
@@ -1,10 +1,34 @@
 public class Game {
 
-    
-
-
     public static void main(String[] args){
 
     }
 
+    /*
+    Schaut ob neue Player connecten wollen und fügt diese der Lobby hinzu, außerdem wird geprueft ob die Player ready sind
+     */
+    public void waitForPlayers(){
+
+    }
+
+    /*
+    Sendet an alle verbunden Player die vom Spielleiter eingerichteten Einstellungen und Trennt sie anschließend.
+     */
+    public void startTheGameAlready(){
+
+    }
+
+    /*
+
+     */
+    public void acceptPlayer(){
+
+    }
+
+    /*
+
+     */
+    public void listenToPlayers(){
+
+    }
 }
