1c30b1991a175b88cf537736fb73047d189664a7
Elias Kia <e.kia@gmx.de>
2018-01-20 13:59:59 +0100

    Updated Game.java

--- server/src/Game.java
+++ server/src/Game.java
@@ -1,10 +1,25 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+
 public class Game {
 
-    public static void main(String[] args){
 
+    private ArrayList<Player> Players = new ArrayList(); //Liste in die alle Player gespeichert werden
+    private int tRatio = 33;
+    private int dRatio = 25;
+    private int prepTime = 30;
+    private int gameTime = 600;
 
+    public Game(){
+        try {
+            ServerSocket ss = new ServerSocket(2307);
+        }
+        catch (IOException e)
+        {
+
+        }
     }
-
     /*
     Schaut ob neue Player connecten wollen und fügt diese der Lobby hinzu, außerdem wird geprueft ob die Player ready sind
      */
@@ -27,9 +42,36 @@ public class Game {
     }
 
     /*
-
+     geht alle Players in einer Schleife durch und prueft ob diese neue Nachrichten vermittelt haben
      */
     public void listenToPlayers(){
+
+    }
+
+    /*
+    Sendet die neuen Daten an alle Player
+    */
+    public void refreshPlayers(){
+
+    }
+
+    public void setDRatio(int dRatio) {
+        this.dRatio = dRatio;
+    }
+
+    public void setTRatio(int tRatio) {
+        this.tRatio = tRatio;
+    }
+
+    public void setPrepTime(int prepTime) {
+        this.prepTime = prepTime;
+    }
+
+    public void setGameTime(int gameTime) {
+        this.gameTime = gameTime;
+    }
+
+    public static void main(String[] args){
 
     }
 }
