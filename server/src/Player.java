c486d96f960191b44ba2475b2a57c0d0fe2b206e
Lars Kadel <lars.kadel@gmx.de>
2018-01-20 13:59:12 +0100

    further Player changes

--- server/src/Player.java
+++ server/src/Player.java
@@ -5,15 +5,20 @@ public class Player {
 
     private Socket socket;
     private String name;
+    private boolean ready;
     private int id;
+    private Game game;
+    private boolean leader;
 
-    public Player(Socket socket, int id){
+    public Player(Socket socket, int id, Game game, boolean leader){
         this.socket = socket;
         this.id = id;
+        this.game = game;
+        this.leader = leader;
         this.name = "";
         try{
+            socket.getOutputStream().write("name".getBytes());
             byte[] tmp = new byte[20];
-
             socket.getInputStream().read(tmp);
             this.name = new String(tmp);
         }catch(IOException e){
@@ -38,7 +43,23 @@ public class Player {
         }
     }
 
-    private void interpretMessage(String msg){
-
+    private void interpretMessage(String msg) {
+        String[] messages = msg.split(";");
+        for (String s : messages) {
+            if (s.matches("^ready")) {
+                ready = true;
+            } else if (s.matches("^notready$")) {
+                ready = false;
+            } else if (s.matches("^tratio:\\d\\d$")  && leader) {
+                game.setTRatio( Integer.parseInt(s.substring(s.indexOf(':')+1), s.indexOf(':')+3));
+            } else if (s.matches("^dratio:\\d\\d$") && leader) {
+                game.setDRatio( Integer.parseInt(s.substring(s.indexOf(':')+1), s.indexOf(':')+3));
+            } else if (s.matches("^preptime:\\d\\d$")  &&  leader) {
+                game.setPrepTime(Integer.parseInt(s.substring(s.indexOf(':')+1), s.indexOf(':')+3));
+            } else if (s.matches("^gametime:\\d\\d$")  && leader) {
+                game.setGameTime( Integer.parseInt(s.substring(s.indexOf(':')+1), s.indexOf(':')+3));
+            }
+        }
+        game.refreshPlayers();
     }
 }
